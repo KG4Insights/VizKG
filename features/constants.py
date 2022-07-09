2ba035087681697ab3b8a03d2ffa74e10b69428d
Vitico99 <nenevictor99@gmail.com>
2022-07-09 09:02:51 -0400

    Add file with constants definitions.

---
+++ features/constants.py
@@ -0,0 +1,13 @@
+# Columns of the result dataset
+FID = 'fid'
+FIELD_ID = 'field_id'
+TRACE_TYPE = 'trace_type'
+IS_XSRC = 'is_xsrc'
+IS_YSRC = 'is_ysrc'
+DATA = 'data'
+DTYPE = 'dtype'
+
+
+N_XSRC = 'n_xsrc'
+N_YSRC = 'n_ysrc'
+N_TRACES = 'n_traces'
